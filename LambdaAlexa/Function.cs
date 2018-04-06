c941575e2eb8464bd936c14772c4bc3792ef9ca9
vaidaabelkyte <vaidilutia@gmail.com>
2018-04-06 16:31:40 +0100

    First Lambda function for simple user Interaction

---
+++ LambdaAlexa/Function.cs
@@ -0,0 +1,59 @@
+using System.Net.Http;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace LambdaAlexa
+{
+    public class Function
+    {
+
+        public const string INVOCATION_NAME = "country detail";
+
+        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
+        {
+
+            var requestType = input.GetRequestType();
+            if (requestType == typeof(IntentRequest))
+            {
+                return MakeSkillResponse(
+                        $"Hello! This is the first skill developed bu Vaida using c sharp.",
+                        true);
+            }
+            else
+            {
+                return MakeSkillResponse(
+                        $"I don't know how to handle this intent. Please say something like Alexa, ask {INVOCATION_NAME} about Ireland.",
+                        true);
+            }
+        }
+
+
+        private SkillResponse MakeSkillResponse(string outputSpeech,
+            bool shouldEndSession,
+            string repromptText = "Just say, tell me about Ireland to learn more. To exit, say, exit.")
+        {
+            var response = new ResponseBody
+            {
+                ShouldEndSession = shouldEndSession,
+                OutputSpeech = new PlainTextOutputSpeech { Text = outputSpeech }
+            };
+
+            if (repromptText != null)
+            {
+                response.Reprompt = new Reprompt() { OutputSpeech = new PlainTextOutputSpeech() { Text = repromptText } };
+            }
+
+            var skillResponse = new SkillResponse
+            {
+                Response = response,
+                Version = "1.0"
+            };
+            return skillResponse;
+        }
+    }
+}
